a37850612780ee1153f82f95b86bbdcd1d80cb5e
Nekoz0101 <148434332+Nekoz0101@users.noreply.github.com>
2023-10-20 13:35:04 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -13,7 +13,7 @@ st.sidebar.header('User Input Parameters')
 def user_input_features():
     TV = st.sidebar.slider('TV', 16.0, 150.0, 250.0 )
     Radio = st.sidebar.slider('Radio', 5.0, 38.0,50.0 )
-    Newspaper = st.sidebar.slider('Petal length', 40.0, 55.0, 65.0)
+    Newspaper = st.sidebar.slider('Newspaper', 40.0, 55.0, 65.0)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
